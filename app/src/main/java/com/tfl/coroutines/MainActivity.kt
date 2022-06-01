7f826bf122ae691f88ff740cd35d628938b3f56d
Victor Yamchinov <kodiers@gmail.com>
2022-06-02 00:18:23 +0300

    Initial commit

---
+++ app/src/main/java/com/tfl/coroutines/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.tfl.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.tfl.coroutines.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        loadCity { it ->
+            binding.tvLocation.text = it
+            loadTemperature(it) {
+                binding.tvTemperature.text = it.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            Toast.makeText(this,
+                getString(R.string.loading_temperature_toast, city),
+                Toast.LENGTH_SHORT)
+                .show()
+            Thread.sleep(5000)
+            callback.invoke(17)
+        }
+
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Moscow")
+        }
+    }
+}
